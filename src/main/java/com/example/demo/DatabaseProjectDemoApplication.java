32683b91621c01fa0195bdb9f6399644f827a26c
omutwar <95971202+omutwar@users.noreply.github.com>
2025-01-12 18:27:13 -0600

    Project initial commit

---
+++ src/main/java/com/example/demo/DatabaseProjectDemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseProjectDemoApplication {
+	private static final Logger log = LoggerFactory.getLogger(DatabaseProjectDemoApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseProjectDemoApplication.class, args);
+		log.info("### Spring Application Has Been Initialized ###");
+	}
+
+}
